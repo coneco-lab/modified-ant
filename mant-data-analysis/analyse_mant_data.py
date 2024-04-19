eb3d6cec42dcd79b44cf44b39000070993597e5f
Matteo <matteo.dematola@unitn.it>
2024-04-20 01:07:05 +0200

    now plots both group and subject data

--- mant-data-analysis/analyse_mant_data.py
+++ mant-data-analysis/analyse_mant_data.py
@@ -9,6 +9,57 @@ import analysis_config as config
 working_dir = Path.cwd()
 figures_dir = Path(working_dir / "figures")
 
+for subject_number in range(1,config.SAMPLE_SIZE+1):
+    if subject_number < 10:
+        subject_id = f"sub-0{subject_number}"
+    else:
+        subject_id = f"sub-{subject_number}"
+    figures_savedir = utils.set_output_directories(figures_dir=figures_dir,
+                                                   subject=subject_id,
+                                                   group=False)
+
+    mant_data, _ = utils.read_mant_data(data_dir=config.data_dir + f"/{subject_id}",
+                                        trials_per_subject=config.TRIALS_PER_SUBJECT)
+
+    separate_conditions_data = utils.fetch_mant_conditions(all_trials=mant_data)
+    descriptives_dataframes = utils.get_condition_descriptives(conditions=separate_conditions_data,
+                                                               condition_names=config.abbreviated_condition_names)
+
+    for plot_title, plot_type in zip(config.plot_titles, config.plot_types):
+        utils.plot_reaction_times(title=plot_title + f" ({subject_id})",
+                                  conditions=separate_conditions_data,
+                                  condition_names=config.condition_names,
+                                  figures_savedir=figures_savedir,
+                                  plot_type=plot_type)
+
+    utils.plot_rt_over_conditions(conditions=separate_conditions_data,
+                                  condition_names=config.abbreviated_condition_names,
+                                  data_id=subject_id,
+                                  sample_size=None,
+                                  figures_savedir=figures_savedir)
+
+    blockwise_ordered_rts = utils.order_conditions_blockwise(mant_data=mant_data,
+                                                             condition_names=config.abbreviated_condition_names,
+                                                             number_of_blocks=config.NUMBER_OF_BLOCKS,
+                                                             trials_per_block=config.TRIALS_PER_BLOCK)
+
+    utils.plot_blockwise_boxplots(data_id=subject_id,
+                                  sample_size=None,
+                                  blockwise_ordered_rts=blockwise_ordered_rts,
+                                  figures_savedir=figures_savedir)
+
+    relevant_data = utils.get_only_cues_and_targets(mant_data=mant_data)
+    for variable in ["cues","targets"]:
+        utils.plot_target_cue_interactions(mant_data=relevant_data,
+                                           data_id=subject_id,
+                                           on_x_axis=variable,
+                                           sample_size=None,
+                                           figures_savedir=figures_savedir)
+
+############################################################################
+### now do the  same things, but at group level (plus statistical tests) ###
+############################################################################
+
 figures_savedir = utils.set_output_directories(figures_dir=figures_dir,
                                                subject=None,
                                                group=True)
@@ -29,6 +80,7 @@ for plot_title, plot_type in zip(config.plot_titles, config.plot_types):
 
 utils.plot_rt_over_conditions(conditions=separate_conditions_data,
                               condition_names=config.abbreviated_condition_names,
+                              data_id="group",
                               sample_size=sample_size,
                               figures_savedir=figures_savedir)
 
@@ -37,7 +89,8 @@ blockwise_ordered_rts = utils.order_conditions_blockwise(mant_data=mant_data,
                                                          number_of_blocks=config.NUMBER_OF_BLOCKS,
                                                          trials_per_block=config.TRIALS_PER_BLOCK)
 
-utils.plot_blockwise_boxplots(sample_size=sample_size,
+utils.plot_blockwise_boxplots(data_id="group",
+                              sample_size=sample_size,
                               blockwise_ordered_rts=blockwise_ordered_rts,
                               figures_savedir=figures_savedir)
 
