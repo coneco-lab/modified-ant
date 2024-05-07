4f4551d9e72243d40aadb7604b971d29562231f3
Matteo <matteo.dematola@unitn.it>
2024-05-07 18:32:03 +0200

    created one subdirectory per experiment

---
+++ task-and-eeg/config.py
@@ -0,0 +1,310 @@
+import random
+
+from psychopy import monitors, visual
+   
+conditions_file = "mant-conditions.csv"
+NUMBER_OF_BLOCKS = 9
+TRIALS_PER_BLOCK = 72
+MAX_TRIAL_DURATION = 3.6
+
+output_variables = ["cue_location",
+                    "sequence_location",
+                    "cue_type",
+                    "target_congruent",
+                    "target_direction",
+                    "response",
+                    "correct",
+                    "rt"]
+monitor_info = {"name": "LA2205-monitor-eeglab", #"hp-monitor-eeglab",
+                 "size_pixels": [1600,900],      #[1920,1080],
+                 "width_cm": (47.5),             #(62),
+                 "subject_distance_cm": 65,
+                 "refresh_rate_hz": 59}
+monitor = monitors.Monitor(name=monitor_info["name"])
+monitor.setSizePix(monitor_info["size_pixels"]) 
+monitor.setWidth(monitor_info["width_cm"]) 
+monitor.setDistance(monitor_info["subject_distance_cm"]) 
+monitor.saveMon()
+
+window = visual.Window(size=monitor.getSizePix(),
+                       fullscr=True, 
+                       screen=0,
+                       winType="pyglet", 
+                       allowStencil=False,
+                       monitor=monitor.name,
+                       color=[0.0039, 0.0039, 0.0039],
+                       colorSpace="rgb",
+                       #backgroundImage="", 
+                       #backgroundFit="none",
+                       blendMode="avg", 
+                       useFBO=True,
+                       units="deg")
+
+display_times = {"instructions": 180,
+                 "fixation_demo": 10,
+                 "cue_demo": 10,
+                 "arrows_demo": 10,        
+                 "initial_fixation": [0.001*random.randrange(500,1000,1) for trial in range(TRIALS_PER_BLOCK)], 
+                 "cue": 0.1,
+                 "later_fixation": 1.1,                                                                       
+                 "target": 1.1,
+                 "end_of_block_text": 3600}                                      
+
+frames_per_item = {"instructions":int(display_times["instructions"]*monitor_info["refresh_rate_hz"]),
+                   "fixation_demo": int(display_times["fixation_demo"]*monitor_info["refresh_rate_hz"]),
+                   "cue_demo": int(display_times["cue_demo"]*monitor_info["refresh_rate_hz"]),
+                   "arrows_demo": int(display_times["arrows_demo"]*monitor_info["refresh_rate_hz"]),
+                   "initial_fixation": [int(round(time*monitor_info["refresh_rate_hz"])) for time in display_times["initial_fixation"]],
+                   "cue": int(display_times["cue"]*monitor_info["refresh_rate_hz"]),
+                   "later_fixation": int(display_times["later_fixation"]*monitor_info["refresh_rate_hz"]),                              
+                   "target": int(display_times["target"]*monitor_info["refresh_rate_hz"]),
+                   "end_of_block_text": int(display_times["end_of_block_text"]*monitor_info["refresh_rate_hz"])}
+
+trigger_codes = {"spatial_valid_cue": 2,
+                 "spatial_invalid_cue": 3,
+                 "double_cue": 4,
+                 "congruent_target": 5,
+                 "incongruent_target": 6,
+                 "response": 7}                
+
+# fixation cross vertices in PsychoPy window space
+fixation_vertices = [[0.275,1.035-1.06],                                
+                     [0.275,1.085-1.06],
+                     [0.025,1.085-1.06],
+                     [0.025,1.335-1.06],
+                     [-0.025,1.335-1.06],
+                     [-0.025,1.085-1.06],
+                     [-0.275,1.085-1.06],
+                     [-0.275,1.035-1.06],
+                     [-0.025,1.035-1.06],
+                     [-0.025,0.785-1.06],
+                     [0.025,0.785-1.06],
+                     [0.025,1.035-1.06]]
+
+fixation = visual.ShapeStim(win=window,
+                            name="fixation_cross",
+                            vertices=fixation_vertices,
+                            units="deg",
+                            size=(1,1),
+                            ori=0.0,
+                            pos=(0,0),
+                            anchor="center",
+                            lineWidth=1.0,
+                            colorSpace="rgb",
+                            lineColor="black",
+                            fillColor="black",
+                            opacity=None,
+                            depth=0.0,
+                            interpolate=True)
+
+# the asterisk cue is actually an overlap of four lines. 
+# each line has a start and an end that will be defined iteratively on every trial because the cue's position varies across trials  
+
+# vertical line
+cue1_vertical = visual.line.Line(window,
+                                name="cue1_vertical",
+                                units="deg",
+                                size=(1,1),
+                                start=(None,None),
+                                end=(None,None),
+                                ori=0.0,
+                                pos=(0,0),
+                                anchor="center",
+                                lineWidth=2.0,
+                                colorSpace="rgb",
+                                lineColor="black",
+                                interpolate=True)
+
+# horizontal line
+cue1_horizontal = visual.line.Line(window,
+                                  name="cue1_horizontal",
+                                  units="deg",
+                                  size=(1,1),
+                                  start=(None,None),
+                                  end=(None,None),
+                                  ori=0.0,
+                                  pos=(0,0),
+                                  anchor="center",
+                                  lineWidth=2.0,
+                                  colorSpace="rgb",
+                                  lineColor="black",
+                                  interpolate=True)
+
+# first oblique line (top right to bottom left)
+cue1_rightleft = visual.line.Line(window,
+                                 name="cue1_rightleft",
+                                 units="deg",
+                                 size=(1,1),
+                                 start=(None,None),
+                                 end=(None,None),
+                                 ori=0.0,
+                                 pos=(0,0),
+                                 anchor="center",
+                                 lineWidth=2,
+                                 colorSpace="rgb",
+                                 lineColor="black",
+                                 interpolate=True)
+
+# second oblique line (top left to bottom right)
+cue1_leftright = visual.line.Line(window,
+                                 name="cue1_leftright",
+                                 units="deg",
+                                 size=(1,1),
+                                 start=(None,None),
+                                 end=(None,None),
+                                 ori=0.0,
+                                 pos=(0,0),
+                                 anchor="center",
+                                 lineWidth=2,
+                                 colorSpace="rgb",
+                                 lineColor="black",
+                                 interpolate=True)
+
+# vertical line
+cue2_vertical = visual.line.Line(window,
+                                 name="cue2_vertical",
+                                 units="deg",
+                                 size=(1,1),
+                                 start=(None,None),
+                                 end=(None,None),
+                                 ori=0.0,
+                                 pos=(0,0),
+                                 anchor="center",
+                                 lineWidth=2.0,
+                                 colorSpace="rgb",
+                                 lineColor="black",
+                                 interpolate=True)
+
+# horizontal line
+cue2_horizontal = visual.line.Line(window,
+                                   name="cue2_horizontal",
+                                   units="deg",
+                                   size=(1,1),
+                                   start=(None,None),
+                                   end=(None,None),
+                                   ori=0.0,
+                                   pos=(0,0),
+                                   anchor="center",
+                                   lineWidth=2.0,
+                                   colorSpace="rgb",
+                                   lineColor="black",
+                                   interpolate=True)
+
+# first oblique line (top right to bottom left)
+cue2_rightleft = visual.line.Line(window,
+                                  name="cue2_rightleft",
+                                  units="deg",
+                                  size=(1,1),
+                                  start=(None,None),
+                                  end=(None,None),
+                                  ori=0.0,
+                                  pos=(0,0),
+                                  anchor="center",
+                                  lineWidth=2,
+                                  colorSpace="rgb",
+                                  lineColor="black",
+                                  interpolate=True)
+
+# second oblique line (top left to bottom right)
+cue2_leftright = visual.line.Line(window,
+                                  name="cue2_leftright",
+                                  units="deg",
+                                  size=(1,1),
+                                  start=(None,None),
+                                  end=(None,None),
+                                  ori=0.0,
+                                  pos=(0,0),
+                                  anchor="center",
+                                  lineWidth=2,
+                                  colorSpace="rgb",
+                                  lineColor="black",
+                                  interpolate=True)
+
+# leftmost flanker
+flanker1 = visual.ShapeStim(window, 
+                            name="flanker1", 
+                            units="deg",
+                            size=(1,1),
+                            vertices=None,
+                            ori=0.0,
+                            pos=(0,0),
+                            anchor="center",
+                            lineWidth=1.0,
+                            colorSpace="rgb",
+                            lineColor="black",
+                            fillColor="black",
+                            interpolate=True)
+
+# second-from-left flanker
+flanker2 = visual.ShapeStim(window, 
+                            name="flanker2", 
+                            units="deg",
+                            size=(1,1),
+                            vertices=None,
+                            ori=0.0,
+                            pos=(0,0),
+                            anchor="center",
+                            lineWidth=1.0,
+                            colorSpace="rgb",
+                            lineColor="black",
+                            fillColor="black",
+                            interpolate=True)
+   
+target = visual.ShapeStim(window, 
+                          name="target", 
+                          units="deg",
+                          size=(1,1),
+                          vertices=None,
+                          ori=0.0,
+                          pos=(0,0),
+                          anchor="center",
+                          lineWidth=1.0,
+                          colorSpace="rgb",
+                          lineColor="black",
+                          fillColor="black",
+                          interpolate=True)
+
+# second-from-right flanker
+flanker3 = visual.ShapeStim(window, 
+                            name="flanker3", 
+                            units="deg",
+                            size=(1,1),
+                            vertices=None,
+                            ori=0.0,
+                            pos=(0,0),
+                            anchor="center",
+                            lineWidth=1.0,
+                            colorSpace="rgb",
+                            lineColor="black",
+                            fillColor="black",
+                            interpolate=True)
+
+# rightmost flanker
+flanker4 = visual.ShapeStim(window, 
+                            name="flanker4", 
+                            units="deg",
+                            size=(1,1),
+                            vertices=None,
+                            ori=0.0,
+                            pos=(0,0),
+                            anchor="center",
+                            lineWidth=1.0,
+                            colorSpace="rgb",
+                            lineColor="black",
+                            fillColor="black",
+                            interpolate=True)
+
+asterisk_components = [cue1_vertical, 
+                       cue1_horizontal,
+                       cue1_rightleft,
+                       cue1_leftright,
+                       cue2_vertical, 
+                       cue2_horizontal,
+                       cue2_rightleft,
+                       cue2_leftright]
+                 
+arrows = [flanker1,
+          flanker2,
+          target, 
+          flanker3,
+          flanker4]
---
+++ task-and-eeg/master_script.py
@@ -0,0 +1,85 @@
+import psychopy
+#psychopy.useVersion("2022.2.5")
+from psychopy import core, data, gui, parallel
+
+experiment_info = {"name": "mANT",
+                   "subject": "",
+                   "session": "",
+                   "date": data.getDateStr(),
+                   "psychopy_version": "2022.2.5"}
+
+dialogue = gui.DlgFromDict(dictionary=experiment_info,
+                           title="Enter subject ID (e.g., S01) and session ID (e.g., 01)",
+                           sortKeys=False)
+del dialogue
+
+if experiment_info["subject"] != "": # start experiment only if subject ID has been provided
+    
+    import pandas as pd
+    import utils
+    import config
+     
+    output_tables = utils.set_up_output(number_of_blocks=config.NUMBER_OF_BLOCKS,
+                                        number_of_trials=config.TRIALS_PER_BLOCK,
+                                        output_variables=config.output_variables)
+
+    text_folder, beh_data_folder = utils.make_directories(experiment_info=experiment_info)  
+    
+    conditions = data.importConditions(config.conditions_file)
+    response_clock = core.Clock()
+    port = parallel.ParallelPort(address="0x0378") 
+
+    utils.display_text(file_to_read=text_folder / "welcome-message.txt", 
+                       window=config.window,
+                       display_duration=config.frames_per_item["instructions"])
+
+    demos_to_display = [config.fixation,
+                        config.asterisk_components,
+                        config.arrows] 
+    demos_frames = [config.frames_per_item["fixation_demo"], 
+                    config.frames_per_item["cue_demo"], 
+                    config.frames_per_item["arrows_demo"]]
+    utils.display_demos(trials_pool=conditions,
+                        window=config.window,
+                        demos=demos_to_display,
+                        demos_frames = demos_frames)
+    del demos_to_display, demos_frames
+
+    _ = utils.display_text(file_to_read=text_folder / "post-demo-message.txt", 
+                           window=config.window,
+                           display_duration=config.frames_per_item["instructions"])
+    
+    training_trials = data.TrialHandler(trialList=conditions, 
+                                        nReps=1)
+                                        
+    utils.run_trials_save_data(trials=training_trials,
+                               elapsed_trials=0,
+                               response_clock=response_clock,
+                               beh_data_folder=beh_data_folder,
+                               experiment_info=experiment_info,
+                               port=port)
+    
+    _ = utils.display_text(file_to_read=text_folder / "post-training-message.txt", 
+                           window=config.window,
+                           display_duration=config.frames_per_item["instructions"])
+    
+    for block_number in range(config.NUMBER_OF_BLOCKS):
+        experimental_trials = data.TrialHandler(trialList=conditions, 
+                                                nReps=int(config.TRIALS_PER_BLOCK / len(conditions)))
+        elapsed_trials = config.TRIALS_PER_BLOCK*(block_number)                                        
+        utils.run_trials_save_data(trials=experimental_trials,
+                                   elapsed_trials=elapsed_trials,
+                                   response_clock=response_clock,
+                                   beh_data_folder=beh_data_folder,
+                                   experiment_info=experiment_info,
+                                   port=port)
+        
+        decision_after_block = utils.display_text(file_to_read=text_folder / "end-of-block-message.txt",
+                                                  window=config.window,
+                                                  display_duration=config.frames_per_item["end_of_block_text"])
+        if decision_after_block == "escape":
+            break
+
+    _ = utils.display_text(file_to_read=text_folder / "farewell-message.txt", 
+                           window=config.window,
+                           display_duration=config.frames_per_item["instructions"])
